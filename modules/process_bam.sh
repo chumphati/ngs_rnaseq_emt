2e8b578840122534ab3642345f87535e687a7140
Fiona Hak <fiona.hak@universite-paris-saclay.fr>
2023-09-27 21:19:33 +0200

    update bugs index2

--- modules/process_bam.sh
+++ modules/process_bam.sh
@@ -15,18 +15,18 @@ x=0
 for i in ${sample_list[*]}; do
   ((x=x+15))
   echo -e "\033[1AIndex bam files ............................. ${x}%"
-  samtools index "$MAPPING/${i}Aligned.sortedByCoord.out.bam"
+  samtools index "$MAPPING/${i}Aligned.sortedByCoord.out.bam" > /dev/null 2>&1
 done
 echo -e "\033[1AIndex bam files ............................. 100%"
 
 echo "Feature counts ............................. 0%"
-featureCounts -p -t exon -g gene_id -a "$MAPPING_REF/gencode.v24lift37.basic.annotation.gtf" -o "$COUNT/featureCounts.txt" "$MAPPING/${i}*.bam"
+featureCounts -p -t exon -g gene_id -a "$MAPPING_REF/gencode.v24lift37.basic.annotation.gtf" -o "$COUNT/featureCounts.txt" "$MAPPING/*.bam" > /dev/null 2>&1
 echo -e "\033[1AFeature counts ............................. 25%"
-perl -ne 'print "$1 $2\n" if /gene_id \"(.*?)\".*gene_name \"(.*?)\"/' "$MAPPING_REF/gencode.v24lift37.basic.annotation.gtf" | sort | uniq > "$COUNT/encode-to-hugo.tab"
+perl -ne 'print "$1 $2\n" if /gene_id \"(.*?)\".*gene_name \"(.*?)\"/' "$MAPPING_REF/gencode.v24lift37.basic.annotation.gtf" | sort | uniq > "$COUNT/encode-to-hugo.tab" > /dev/null 2>&1
 echo -e "\033[1AFeature counts ............................. 50%"
-sort -k1,1 "$COUNT/featureCounts.txt" > "$COUNT/temp1"
+sort -k1,1 "$COUNT/featureCounts.txt" > "$COUNT/temp1" > /dev/null 2>&1
 echo -e "\033[1AFeature counts ............................. 60%"
-sort -k1,1 "$COUNT/encode-to-hugo.tab" > "$COUNT/temp2"
+sort -k1,1 "$COUNT/encode-to-hugo.tab" > "$COUNT/temp2" > /dev/null 2>&1
 echo -e "\033[1AFeature counts ............................. 80%"
-join -1 1 -2 1 "$COUNT/temp1" "$COUNT/temp2" | grep "chr18" | awk '{print $5 " " $1}' > "$COUNT/final_counts.txt"
+join -1 1 -2 1 "$COUNT/temp1" "$COUNT/temp2" | grep "chr18" | awk '{print $5 " " $1}' > "$COUNT/final_counts.txt" > /dev/null 2>&1
 echo -e "\033[1AFeature counts ............................. 100%"
